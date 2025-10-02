f87dc46936537d9fcd49252b2d0c854f26320eb1
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2025-10-02 11:25:04 +0200

    Add src/data/portfolioData.js

---
+++ src/data/portfolioData.js
@@ -0,0 +1,20 @@
+// This file will be dynamically generated with user data
+const portfolioData = {
+  header: {
+    name: "Cleo Qin",
+    title: "Professional",
+    summary: "Postgraduate Student"
+  },
+  skills: ["Curiosity and eagerness to learn new things","Willingness to expand current knowledge","Dynamic: Ability to adapt and adjust to changing situations","Good communication skills","Team player: Ability to work in groups"],
+  experience: [{"title":"Multimedia Editor","company":"PDBY Media","startDate":"Aug 2023","endDate":"Mar 2024","extra":["Managed a team that creates content for web, social media and multimedia. Attended final checks sessions to compile and complete each edition."]},{"title":"Multimedia Journalist","company":"PDBY Media","startDate":"Aug 2022","endDate":"Mar 2024","extra":["Created visuals via various mediums and covered planned and spontaneous events to capture moments through a camera lens."]},{"title":"Academic Tutor","company":"University of Pretoria","startDate":"Mar 2024","endDate":"present","extra":["Tutor first-year students in Social Anthropology, create and facilitate original tutorial sessions, host student consultations, mark assignments and examinations."]},{"title":"Private Tutor","company":"Prep2Perfection","startDate":"May 2022","endDate":"May 2024","extra":["Tutored primary school and high school students, provided personalised instruction to ensure each individual student achieves their highest academic potential."]}],
+  education: [{"institution":"Midstream College High School","degree":"Matric Certificate","field":"","endDate":"2017-2021","gpa":""},{"institution":"University of Pretoria","degree":"Undergraduate BA","field":"Psychology and Anthropology","endDate":"2022-2024","gpa":""},{"institution":"University of Pretoria","degree":"BSocSci HONOURS","field":"","endDate":"","gpa":""}],
+  projects: [],
+  contact: {
+    email: "",
+    phone: "",
+    linkedin: "",
+    github: ""
+  }
+};
+
+export default portfolioData;
